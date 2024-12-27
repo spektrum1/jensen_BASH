91428674d3fda9831089c74210013775a2e36936
spektrum1 <viktor.altintas@gmail.com>
2024-12-27 11:38:54 +0100

    add RUN_NUMBER env var and conditional statement

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ -z "$RUN_NUMBER" ]; then
+  export RUN_NUMBER=1
+else
+  export RUN_NUMBER=$((RUN_NUMBER + 1))
+fi
+
 DATE=$(date)
 echo run number: $RUN_NUMBER : $DATE > output.txt
