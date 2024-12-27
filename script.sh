3483f4e2243d51f08271d1411e62499d88777acd
spektrum1 <viktor.altintas@gmail.com>
2024-12-27 11:47:50 +0100

    change RUN_NUMBER env to log file

--- script.sh
+++ script.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-if [ -z "$RUN_NUMBER" ]; then
-  export RUN_NUMBER=1
-else
-  export RUN_NUMBER=$((RUN_NUMBER + 1))
+COUNTER_PATH="${HOME}/var/log/run_counter/run_number.log"
+OUTPUT_PATH="${HOME}/var/log/run_counter/output.log"
+
+if [ ! -f $COUNTER_PATH ];
+then
+    echo 0 > $COUNTER_PATH
 fi
 
+RUN_NUMBER=$(cat $COUNTER_PATH)
+
+RUN_NUMBER=$((RUN_NUMBER + 1))
+
+echo $RUN_NUMBER > $COUNTER_PATH
+
 DATE=$(date)
-echo run number: $RUN_NUMBER : $DATE > output.txt
+
+echo run number: $RUN_NUMBER : $DATE >> $OUTPUT_PATH
+echo run number: $RUN_NUMBER : $DATE
